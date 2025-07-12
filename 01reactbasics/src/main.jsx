7269f4af47296351be31958329838b08957f5520
Sahil Raturi <sahilraturi687@gmail.com>
2025-07-12 19:26:27 +0530

    create your own react

--- 01reactbasics/src/main.jsx
+++ 01reactbasics/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+
+
+function App() {
+  return (
+   <h1>Hello world</h1>
+  )
+}
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
