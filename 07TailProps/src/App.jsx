83c4b396021c4640198fc9e8770ad588d35e65a3
Sahil Raturi <sahilraturi687@gmail.com>
2025-07-13 14:41:19 +0530

    Tailwind and props

---
+++ 07TailProps/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Nft from './components/Nft'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-2 rounded-3xl'>Tailwind at your service ladies and gentelman</h1>
+      <Nft theme = "moving" />
+    </>
+  )
+}
+
+export default App
