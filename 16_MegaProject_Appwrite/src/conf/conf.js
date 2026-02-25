5bdc22ea6921d8b284e43af168968d582c7f307b
Sahil Raturi <sahilraturi687@gmail.com>
2026-02-25 12:18:24 +0530

    remove .gitignore from all subfolders

---
+++ 16_MegaProject_Appwrite/src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteuUrl: String(import.meta.env.VITE_APPWRITE_KEY),
+    appwriteuUrl: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteuUrl: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteuUrl: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteuUrl: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+
+export default conf
